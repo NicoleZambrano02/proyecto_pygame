9c571c3dde2f84a31ff623f6bfbbd6821b55c0b0
KevinJ1998 <kjsegovia144@hotmail.es>
2019-07-05 09:29:52 -0500

    CAMBIO DEL ASKLDJFLSADKGH

--- game0.py
+++ game0.py
@@ -1,13 +1,12 @@
-fondo='bomberman.jpg'
-personaje='personaje.png'
 import pygame
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
-
+fondo='bomberman.jpg'
+personaje='personaje.png'
 screen=pygame.display.set_mode((102,819),0,32)
-pygame.display.set_caption("BIENVENIDO A MI JUEGO")
+pygame.display.set_caption("BIENVENIDO A MI JUEGO :vv")
 background=pygame.image.load(fondo).convert()
 mouse_cursor=pygame.image.load(personaje).convert_alpha()
 
@@ -22,5 +21,6 @@ while True:
 		y-=mouse_cursor.get_height()/2
 		screen.blit(mouse_cursor,(x,y))
 		pygame.display.update()
-#hola pipol
+#hola pipol jaguar llu :vv
 # que mas pex
+"""estoy hasta la lptm del pull y los huevos >:vv
